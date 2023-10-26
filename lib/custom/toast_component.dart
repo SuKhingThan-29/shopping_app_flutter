612471c53baaef72f3efb968c9ecce7b183e6786
KaungSackNaing <kaungsack1512@gmail.com>
2023-10-26 16:08:37 +0630

    ksn

--- lib/custom/toast_component.dart
+++ lib/custom/toast_component.dart
@@ -1,4 +1,3 @@
-
 import 'package:toast/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:active_ecommerce_flutter/my_theme.dart';
@@ -7,28 +6,36 @@ import 'package:one_context/one_context.dart';
 class ToastComponent {
   static showDialog(String msg, {duration = 0, gravity = 0}) {
     ToastContext().init(OneContext().context!);
-    Toast.show(
-      msg,
-      duration: duration != 0 ? duration : Toast.lengthShort,
-      gravity: gravity != 0 ? gravity : Toast.bottom,
-        backgroundColor:Colors.black,
+    Toast.show(msg,
+        duration: duration != 0 ? duration : Toast.lengthShort,
+        gravity: gravity != 0 ? gravity : Toast.bottom,
+        backgroundColor: Colors.black,
         textStyle: TextStyle(color: Colors.white),
         border: Border(
             top: BorderSide(
               color: Color.fromRGBO(203, 209, 209, 1),
-            ),bottom:BorderSide(
-          color: Color.fromRGBO(203, 209, 209, 1),
-        ),right: BorderSide(
-          color: Color.fromRGBO(203, 209, 209, 1),
-        ),left: BorderSide(
-          color: Color.fromRGBO(203, 209, 209, 1),
-        )),
-        backgroundRadius: 6
-    );
+            ),
+            bottom: BorderSide(
+              color: Color.fromRGBO(203, 209, 209, 1),
+            ),
+            right: BorderSide(
+              color: Color.fromRGBO(203, 209, 209, 1),
+            ),
+            left: BorderSide(
+              color: Color.fromRGBO(203, 209, 209, 1),
+            )),
+        backgroundRadius: 6);
   }
-  static showSnackBar(BuildContext context,String ms){
+
+  static showSnackBar(BuildContext context, String ms) {
     return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('$ms')),
+      SnackBar(
+        content: Text('$ms'),
+        dismissDirection: DismissDirection.up,
+        behavior: SnackBarBehavior.floating,
+        margin:
+            EdgeInsets.only(bottom: MediaQuery.of(context).size.height - 150),
+      ),
     );
   }
 }
