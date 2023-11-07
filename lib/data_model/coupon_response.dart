42e39720282b9e84e48e9a8a4761b7a9f8bae3c5
KaungSackNaing <kaungsack1512@gmail.com>
2023-11-07 15:03:53 +0630

    ksn

---
+++ lib/data_model/coupon_response.dart
@@ -0,0 +1,181 @@
+// To parse this JSON data, do
+//
+//     final coupon = couponFromJson(jsonString);
+
+import 'dart:convert';
+
+Coupon couponFromJson(String str) => Coupon.fromJson(json.decode(str));
+
+String couponToJson(Coupon data) => json.encode(data.toJson());
+
+class Coupon {
+  List<Datum>? data;
+  Links links;
+  Meta meta;
+
+  Coupon({
+    this.data,
+    required this.links,
+    required this.meta,
+  });
+
+  factory Coupon.fromJson(Map<String, dynamic> json) => Coupon(
+        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+        links: Links.fromJson(json["links"]),
+        meta: Meta.fromJson(json["meta"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "data": List<dynamic>.from(data!.map((x) => x.toJson())),
+        "links": links.toJson(),
+        "meta": meta.toJson(),
+      };
+}
+
+class Datum {
+  int? id;
+  String? code;
+  String? type;
+  String? details;
+  int? discount;
+  String? discountType;
+  String? startDate;
+  String? endDate;
+  int? pointAmount;
+  bool? isVerifiedCoupon;
+
+  Datum({
+    this.id,
+    this.code,
+    this.type,
+    this.details,
+    this.discount,
+    this.discountType,
+    this.startDate,
+    this.endDate,
+    this.pointAmount,
+    this.isVerifiedCoupon,
+  });
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        id: json["id"],
+        code: json["code"],
+        type: json["type"],
+        details: json["details"],
+        discount: json["discount"],
+        discountType: json["discount_type"],
+        startDate: json["start_date"],
+        endDate: json["end_date"],
+        pointAmount: json["point_amount"],
+        isVerifiedCoupon: json["is_verified_coupon"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "code": code,
+        "type": type,
+        "details": details,
+        "discount": discount,
+        "discount_type": discountType,
+        "start_date": startDate,
+        "end_date": endDate,
+        "point_amount": pointAmount,
+        "is_verified_coupon": isVerifiedCoupon,
+      };
+}
+
+class Links {
+  String first;
+  String last;
+  dynamic prev;
+  dynamic next;
+
+  Links({
+    required this.first,
+    required this.last,
+    required this.prev,
+    required this.next,
+  });
+
+  factory Links.fromJson(Map<String, dynamic> json) => Links(
+        first: json["first"],
+        last: json["last"],
+        prev: json["prev"],
+        next: json["next"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "first": first,
+        "last": last,
+        "prev": prev,
+        "next": next,
+      };
+}
+
+class Meta {
+  int? currentPage;
+  int? from; // Change to int?
+  int? lastPage;
+  List<Link>? links;
+  String? path;
+  int? perPage;
+  int? to;
+  int? total;
+
+  Meta({
+    this.currentPage,
+    this.from,
+    this.lastPage,
+    this.links,
+    this.path,
+    this.perPage,
+    this.to,
+    this.total,
+  });
+
+  factory Meta.fromJson(Map<String, dynamic> json) => Meta(
+        currentPage: json["current_page"],
+        from: json["from"] as int?, // Use as int?
+        lastPage: json["last_page"],
+        links: List<Link>.from(json["links"].map((x) => Link.fromJson(x))),
+        path: json["path"],
+        perPage: json["per_page"],
+        to: json["to"],
+        total: json["total"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "current_page": currentPage,
+        "from": from,
+        "last_page": lastPage,
+        "links": List<dynamic>.from(links!.map((x) => x.toJson())),
+        "path": path,
+        "per_page": perPage,
+        "to": to,
+        "total": total,
+      };
+}
+
+class Link {
+  String? url;
+  String label;
+  bool active;
+
+  Link({
+    required this.url,
+    required this.label,
+    required this.active,
+  });
+
+  factory Link.fromJson(Map<String, dynamic> json) => Link(
+        url: json["url"],
+        label: json["label"],
+        active: json["active"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url,
+        "label": label,
+        "active": active,
+      };
+}
---
+++ lib/presenter/home_presenter.dart
@@ -0,0 +1,285 @@
+import 'dart:async';
+
+import 'package:active_ecommerce_flutter/custom/toast_component.dart';
+import 'package:active_ecommerce_flutter/helpers/shared_value_helper.dart';
+import 'package:active_ecommerce_flutter/repositories/category_repository.dart';
+import 'package:active_ecommerce_flutter/repositories/flash_deal_repository.dart';
+import 'package:active_ecommerce_flutter/repositories/product_repository.dart';
+import 'package:active_ecommerce_flutter/repositories/sliders_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+import '../repositories/brand_repository.dart';
+
+class HomePresenter extends ChangeNotifier {
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  int current_slider = 0;
+  ScrollController? allProductScrollController;
+  ScrollController? featuredCategoryScrollController;
+  ScrollController mainScrollController = ScrollController();
+
+  late AnimationController pirated_logo_controller;
+  late Animation pirated_logo_animation;
+
+  var carouselImageList = [];
+  var bannerOneImageList = [];
+  var bannerTwoImageList = [];
+  var featuredCategoryList = [];
+  var todaysDealProducts = [];
+  var flashDealProducts = [];
+
+  bool isCategoryInitial = true;
+
+  bool isCarouselInitial = true;
+  bool isBannerOneInitial = true;
+  bool isBannerTwoInitial = true;
+  bool isTodayDetailInitial = true;
+  bool isFlashDealInitial = true;
+
+  var featuredProductList = [];
+  bool isFeaturedProductInitial = true;
+  int? totalFeaturedProductData = 0;
+  int featuredProductPage = 1;
+  bool showFeaturedLoadingContainer = false;
+
+  bool isTodayDeal = false;
+  bool isFlashDeal = false;
+
+  var allBrandList = [];
+  bool isBrandInitial =true;
+  int? totalBrandData = 0;
+  int allBrandPage=1;
+  bool showBrandLoadingContainer = false;
+
+  var allProductList = [];
+  bool isAllProductInitial = true;
+  int? totalAllProductData = 0;
+  int allProductPage = 1;
+  bool showAllLoadingContainer = false;
+  int cartCount = 0;
+  int lastPage=1;
+  String selectedTab="Recommended";
+  bool isScrollData=false;
+  bool isMoreProduct=false;
+
+
+  fetchAll() {
+    fetchCarouselImages();
+    fetchBannerOneImages();
+    fetchBannerTwoImages();
+    fetchFeaturedCategories();
+    fetchFeaturedProducts();
+    fetchAllProducts(tab: selectedTab);
+    fetchTodayDealData();
+    fetchFlashDealData();
+  }
+  setTab(String tab){
+    selectedTab=tab;
+    notifyListeners();
+  }
+
+  handleSelectProductTab(String tab) {
+    selectedTab=tab;
+    resetAllProductList();
+    fetchAllProducts(tab: selectedTab);
+    notifyListeners();
+  }
+
+  fetchTodayDealData() async {
+    var deal = await ProductRepository().getTodaysDealProducts();
+    print(deal.products!.length);
+    if (deal.success! && deal.products!.isNotEmpty) {
+      todaysDealProducts.addAll(deal.products!);
+      isTodayDeal = true;
+      isTodayDetailInitial = false;
+      notifyListeners();
+    }
+  }
+
+  fetchFlashDealData() async {
+    var deal = await FlashDealRepository().getFlashDeals();
+
+    if (deal.success! && deal.flashDeals!.isNotEmpty) {
+      flashDealProducts.addAll(deal.flashDeals!);
+      isFlashDeal = true;
+      isFlashDealInitial = false;
+      notifyListeners();
+    }
+  }
+
+  fetchCarouselImages() async {
+    var carouselResponse = await SlidersRepository().getSliders();
+    carouselResponse.sliders!.forEach((slider) {
+      carouselImageList.add(slider.photo);
+    });
+    isCarouselInitial = false;
+    notifyListeners();
+  }
+
+  fetchBannerOneImages() async {
+    var bannerOneResponse = await SlidersRepository().getBannerOneImages();
+    bannerOneResponse.sliders!.forEach((slider) {
+      bannerOneImageList.add(slider.photo);
+    });
+    isBannerOneInitial = false;
+    notifyListeners();
+  }
+
+  fetchBannerTwoImages() async {
+    var bannerTwoResponse = await SlidersRepository().getBannerTwoImages();
+    bannerTwoResponse.sliders!.forEach((slider) {
+      bannerTwoImageList.add(slider.photo);
+    });
+    isBannerTwoInitial = false;
+    notifyListeners();
+  }
+
+  fetchFeaturedCategories() async {
+    var categoryResponse = await CategoryRepository().getCategories();
+    featuredCategoryList.addAll(categoryResponse.categories!);
+    isCategoryInitial = false;
+    notifyListeners();
+  }
+
+  fetchFeaturedProducts() async {
+    var productResponse = await ProductRepository().getFeaturedProducts(
+      page: featuredProductPage,
+    );
+    featuredProductPage++;
+    featuredProductList.addAll(productResponse.products!);
+    isFeaturedProductInitial = false;
+    totalFeaturedProductData = productResponse.meta!.total;
+    showFeaturedLoadingContainer = false;
+    notifyListeners();
+  }
+
+  fetchAllProducts({String tab = ""}) async {
+    print("Tab Selected $tab");
+    var productResponse;
+
+    if (tab == "New") {
+      productResponse =
+          await ProductRepository().getNewProducts(page: allProductPage);
+      showAllLoadingContainer=false;
+
+
+    } else if(tab == "Recommended") {
+      print("AppLang recom: ${app_language.$}");
+      productResponse =
+          await ProductRepository().getRecommendProducts(page: allProductPage);
+      showAllLoadingContainer=false;
+
+    }
+
+    if (productResponse.products!.isEmpty) {
+      isAllProductInitial = false;
+      showAllLoadingContainer = false;
+      return;
+    }
+    allProductList.addAll(productResponse.products!);
+    isAllProductInitial = false;
+    totalAllProductData = productResponse.meta!.total;
+    showAllLoadingContainer = false;
+    notifyListeners();
+  }
+
+  reset() {
+    carouselImageList.clear();
+    bannerOneImageList.clear();
+    bannerTwoImageList.clear();
+    featuredCategoryList.clear();
+
+    isCarouselInitial = true;
+    isBannerOneInitial = true;
+    isBannerTwoInitial = true;
+    isCategoryInitial = true;
+    cartCount = 0;
+
+    resetFeaturedProductList();
+    resetAllProductList();
+  }
+
+  Future<void> onRefresh() async {
+    reset();
+    fetchAll();
+  }
+
+  resetFeaturedProductList() {
+    featuredProductList.clear();
+    isFeaturedProductInitial = true;
+    totalFeaturedProductData = 0;
+    featuredProductPage = 1;
+    showFeaturedLoadingContainer = false;
+    notifyListeners();
+  }
+
+  resetAllProductList() {
+    allProductList.clear();
+    isAllProductInitial = true;
+    totalAllProductData = 0;
+    allProductPage = 1;
+    showAllLoadingContainer = false;
+    notifyListeners();
+  }
+
+  mainScrollListener() {
+    mainScrollController.addListener(() {
+      //print("position: " + xcrollController.position.pixels.toString());
+      //print("max: " + xcrollController.position.maxScrollExtent.toString());
+
+      if (mainScrollController.position.pixels ==
+          mainScrollController.position.maxScrollExtent) {
+          showAllLoadingContainer = true;
+          notifyListeners();
+          allProductPage++;
+          fetchAllProducts(tab: selectedTab);
+
+
+      }
+    });
+
+    notifyListeners();
+
+  }
+
+  initPiratedAnimation(vnc) {
+    pirated_logo_controller =
+        AnimationController(vsync: vnc, duration: Duration(milliseconds: 2000));
+    pirated_logo_animation = Tween(begin: 40.0, end: 60.0).animate(
+        CurvedAnimation(
+            curve: Curves.bounceOut, parent: pirated_logo_controller));
+
+    pirated_logo_controller.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        pirated_logo_controller.repeat();
+      }
+    });
+
+    pirated_logo_controller.forward();
+  }
+
+  // incrementFeaturedProductPage(){
+  //   featuredProductPage++;
+  //   notifyListeners();
+  //
+  // }
+
+  incrementCurrentSlider(index) {
+    current_slider = index;
+    notifyListeners();
+  }
+
+  // void dispose() {
+  //   pirated_logo_controller.dispose();
+  //   notifyListeners();
+  // }
+  //
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    pirated_logo_controller.dispose();
+    notifyListeners();
+    super.dispose();
+  }
+}
